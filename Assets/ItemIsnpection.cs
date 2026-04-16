ec3ed4c181d9a87619425ef74c0b7fc239fa838c
tommygl2112 <tommygl2112@gmail.com>
2026-04-16 10:28:48 -0600

    rotar inspected item

--- Assets/ItemIsnpection.cs
+++ Assets/ItemIsnpection.cs
@@ -3,24 +3,41 @@ using StarterAssets;
 
 public class ItemIsnpection : MonoBehaviour
 {
-    public ThirdPersonController thirdPersonController;
-    public Interact interact;
-
-    void Awake()
-    {
-        thirdPersonController.enabled = false;
-        interact.canInteract = false;
-    }
+    public GameObject player;
+    private ThirdPersonController thirdPersonController;
+    private StarterAssetsInputs _input;
+    private Interact interact;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        thirdPersonController = player.GetComponent<ThirdPersonController>();
+        _input = player.GetComponent<StarterAssetsInputs>();
+        interact = player.GetComponent<Interact>();
+
+        thirdPersonController.enabled = false;
+        interact.canInteract = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        // if there is an input and camera position is not fixed
+        if (_input.look.sqrMagnitude >= 0.01f)
+        {
+            float rotationSpeed = 100f;
+            float mouseX = _input.look.x;
+            float mouseY = _input.look.y;
+
+            // Rotación horizontal (izquierda/derecha)
+            transform.Rotate(Vector3.up, -mouseX * rotationSpeed * Time.deltaTime, Space.World);
+            // Rotación vertical (arriba/abajo)
+            transform.Rotate(Vector3.right, -mouseY * rotationSpeed * Time.deltaTime, Space.World);
+        }
     }
 }
